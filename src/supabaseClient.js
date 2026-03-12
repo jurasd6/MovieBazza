5105098b362124a97b3d9f5587e872e95863c4d8
jurasd6 <jurekdawidd@gmail.com>
2026-03-12 02:23:43 +0100

    diagram i link do projketu bazy

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'TUTAJ_WKLEISZ_LINK_OD_KUMPLA'
-const supabaseKey = 'TUTAJ_WKLEISZ_KLUCZ_OD_KUMPLA'
+const supabaseUrl = 'https://ishvyypilqfhquhiekhe.supabase.co/'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzaHZ5eXBpbHFmaHF1aGlla2hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjY1MjcsImV4cCI6MjA4ODg0MjUyN30.s93fcCO6ptn6901Nlvl1s09iOcFKOooPPIwh7viaHL8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
